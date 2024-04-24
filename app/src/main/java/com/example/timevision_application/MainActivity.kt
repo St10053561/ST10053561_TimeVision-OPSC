b94234072429eaab4bf04ab81fd375eba8cd3041
ST100535561 <ST10053561@vcconnect.edu.za>
2024-04-24 20:01:02 +0200

    Table Layout basic UI

--- app/src/main/java/com/example/timevision_application/MainActivity.kt
+++ app/src/main/java/com/example/timevision_application/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.timevision_application
 
+import Project
 import android.os.Bundle
+import android.widget.TableLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,12 +12,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main) 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-        //git feaatures
+        setContentView(R.layout.activity_main)
+
+        //initialize the table layout values
+        val projects = listOf(
+            Project("Project 1", "Category 1", "01/01/2021", "Status 1", "StartEnd 1", 1.0f, 1, 1, "Photo 1"),
+            Project("Project 2", "Category 2", "02/02/2022", "Status 2", "StartEnd 2", 2.0f, 2, 2, "Photo 2"),
+            Project("Project 3", "Category 3", "03/03/2023", "Status 3", "StartEnd 3", 3.0f, 3, 3, "Photo 3")
+        )
+
+        val tableLayout = findViewById<TableLayout>(R.id.tableLayout)
+        val tableManager = TableManager(this, tableLayout)
+        tableManager.populateTable(projects)
+
+
     }
 }

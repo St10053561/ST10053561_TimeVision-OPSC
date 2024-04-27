822ec73d884445bfc1869f5dc677347124179021
Tanaka <ST10050487@vcconnect.edu.za>
2024-04-27 19:43:02 +0200

    I added the logo of TimeVision to the login View. I also implemented validations to the Login view. I also implemented the back button to the Register View

--- app/src/main/java/com/example/timevision_application/MainActivity.kt
+++ app/src/main/java/com/example/timevision_application/MainActivity.kt
@@ -2,26 +2,97 @@ package com.example.timevision_application
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnFocusChangeListener
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnFocusChangeListener {
+    //Declaring Input Fields
+    private lateinit var usernameInput: TextInputEditText
+    private lateinit var passwordInput: TextInputEditText
+    lateinit var loginBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // Initialize input fields
+        usernameInput = findViewById(R.id.usernameInput)
+        passwordInput = findViewById(R.id.passwordInput)
+
         val registerButton = findViewById<Button>(R.id.register_btn)
         registerButton.setOnClickListener {
             val intent = Intent(this, Register::class.java)
             startActivity(intent)
+        }
+
+    }
+    //A method to validate the Username
+    private fun validateUsername(): Boolean {
+        var errorMessage: String? = null
+        val username: String = usernameInput.text.toString()
+
+        if (username.isNullOrEmpty()) {
+            errorMessage = "A Username is Required"
+        }
+        if (errorMessage != null) {
+            // Access the TextInputLayout associated with nameInput
+            val nameInputLayout = usernameInput.parent.parent as TextInputLayout
+            nameInputLayout.isErrorEnabled = true
+            nameInputLayout.error = errorMessage
+        }
+
+        return errorMessage == null
+    }
+
+    //A method to validate the Password
+    private fun validatePassword(): Boolean {
+        var errorMessage: String? = null
+        val password: String = passwordInput.text.toString()
+
+        if (password.isNullOrEmpty()) {
+            errorMessage += "A Password is Required"
+        }
+        if (errorMessage != null) {
+            // Access the TextInputLayout associated with nameInput
+            val nameInputLayout = passwordInput.parent.parent as TextInputLayout
+            nameInputLayout.isErrorEnabled = true
+            nameInputLayout.error = errorMessage
+        }
+        return errorMessage == null
+    }
+
+    override fun onFocusChange(view: View, hasFocus: Boolean) {
+        if (view != null) {
+            when (view.id) {
+                R.id.usernameInput -> {
+                    if (hasFocus) {
+                        // Checking if error is enabled and removing it when focused
+                        val nameInputLayout = usernameInput.parent.parent as TextInputLayout
+                        if (nameInputLayout.isErrorEnabled) {
+                            nameInputLayout.isErrorEnabled = false
+                        }
+                    } else {
+                        validateUsername() // Call validation function when losing focus
+                    }
+                }
+
+                R.id.passwordInput -> {
+                    if (hasFocus) {
+                        // Checking if error is enabled and removing it when focused
+                        val nameInputLayout = passwordInput.parent.parent as TextInputLayout
+                        if (nameInputLayout.isErrorEnabled) {
+                            nameInputLayout.isErrorEnabled = false
+                        }
+                    } else {
+                        validatePassword() // Call validation function when losing focus
+                    }
+                }
+            }
         }
     }
 }
